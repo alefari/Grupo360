631eed29a8c862a887af745774664fee24d10c1d
alefari <ale.jf09@gmail.com>
2020-09-01 17:31:01 -0400

    Exportar Ingreos a PDF

--- src/app/components/ingresos/ingresos.component.ts
+++ src/app/components/ingresos/ingresos.component.ts
@@ -5,6 +5,7 @@ import { InventarioService } from 'src/app/services/inventario.service';
 import { Item } from 'src/app/models/item.model';
 import { CategoriasService } from 'src/app/services/categorias.service';
 import { Categoria } from 'src/app/models/categoria.model';
+import * as html2pdf from 'html2pdf.js';
 
 
 @Component({
@@ -45,5 +46,22 @@ export class IngresosComponent implements OnInit {
 //BUSQUEDA CATEGORIA DE FILTRO POR CATEGORIA EN TABLA DE INGRESOS
   regresarCategoria(id: string) {
     return this.inventario.find(item => item.id == id).tipo;
+  }
+
+  descargarPDF() {
+    const opciones = {
+      margin: 1,
+      filename: 'Ingresos.pdf',
+      image: {type: 'jpeg', quality: 1},
+      html2canvas: {},
+      jsPDF: {unit: 'cm', format: 'letter', orientation: 'portrait'}
+    };
+
+    const contenido: Element = document.getElementById('elemento-a-exportar');
+
+    html2pdf()
+      .from(contenido)
+      .set(opciones)
+      .save();
   }
 }
