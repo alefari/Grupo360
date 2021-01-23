83248ac62fe075c2bca5206c845769f8af09710a
jesusThula <thulajesus@hotmail.com>
2021-01-22 20:50:15 -0400

    cambio de location de reportar y agregar hacia admin

---
+++ Client/src/app/modulo_administrador/components/menu-gestioninterna/menu-gestioninterna.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-gestioninterna',
+  templateUrl: './menu-gestioninterna.component.html',
+  styleUrls: ['./menu-gestioninterna.component.css']
+})
+export class MenuGestioninternaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
